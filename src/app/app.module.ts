c5f60d70a2c70caeaa00e36232df824d33d9ad8b
mia <saulperez.sistemas@gmail.com>
2020-11-06 17:44:56 -0600

    se agrego los botones de paginacion , atras ,adelante , ultima y primera

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,15 +8,16 @@ import {ClienteService} from './clientes/cliente.service';
 import {HttpClientModule} from '@angular/common/http';
 import { FormComponent } from './clientes/form.component'; //agrega la coeccion a api
 import {FormsModule} from '@angular/forms';
-import { PaginatorComponent } from './paginator/paginator.component'; ///  esta libreria no avilita para trabajar con formularios
+import { PaginatorComponent } from './paginator/paginator.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ClientesComponent,
-    FormComponent,
     PaginatorComponent,
+    FormComponent
+
 
   ],
   imports: [
